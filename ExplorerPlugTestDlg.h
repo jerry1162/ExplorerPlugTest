c65bf261833ced1f859f7d92bc73eee9269a1f85
jerry1162 <1162328204@qq.com>
2015-11-21 19:52:24 +0800

    FirstVer
    资源管理器拓展

---
+++ ExplorerPlugTestDlg.h
@@ -0,0 +1,34 @@
+
+// ExplorerPlugTestDlg.h : 头文件
+//
+
+#pragma once
+
+
+// CExplorerPlugTestDlg 对话框
+class CExplorerPlugTestDlg : public CDialogEx
+{
+// 构造
+public:
+	CExplorerPlugTestDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_EXPLORERPLUGTEST_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg int OnCreate (LPCREATESTRUCT lpCreateStruct);
+};
